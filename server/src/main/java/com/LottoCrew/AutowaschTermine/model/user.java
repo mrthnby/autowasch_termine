a52d1d5d1000bae60de5ed73c8e3e2487836e206
Hüseyin Tuğşad Şen <tugsad07@gmail.com>
2023-05-03 16:32:31 +0300

    *Base Service Created

---
+++ server/src/main/java/com/LottoCrew/AutowaschTermine/model/user.java
@@ -0,0 +1,25 @@
+package com.LottoCrew.AutowaschTermine.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class user {
+    @Id
+    private String id;
+    private Date createDate = new Date();
+    private String name;
+    private String openingHours;
+    private String price;
+    private String phoneNumber;
+    private String address;
+    private Integer password;
+    private Integer image_id;
+
+}
